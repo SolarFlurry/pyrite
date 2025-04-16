d439c35b3e65e88c9be821fcd3d7e87f388b899d
Seth Fung <sethfung@Seths-MacBook.local>
2025-04-16 10:37:40 +1000

    Pyrite v1

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var search = document.getElementById("search");
+search.addEventListener('click', (event) => {
+    var searchPopup = document.getElementById("search-popup");
+    searchPopup.classList.toggle("show")
+})
